72380f9485e66c912add8c3f4e15cb53cea35b3c
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-07 09:36:25 -0500

    begane work on new conditional exercises and codewars exercises

--- extra exercises/codewar extra exercises/codewar exercise 1.js
+++ extra exercises/codewar extra exercises/codewar exercise 1.js
@@ -1,3 +1,4 @@
+// Population Problem
 // In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
 //
 //     At the end of the first year there will be:
@@ -12,12 +13,15 @@
 // It will need 3 entire years.
 
 function nbYear(p0, percent, aug, p) {
-    percent = (percent/100)
-    if (p <= p0){
-        return n;
+    let years = 0;
+    for (years; p0 < p; years++) {
+        p0 += p0 * (percent / 100) + aug;
     }
-    else{
+    return years;
+};
+//-------------------------------------------------------------------------------------------------------------
+// Tiangle Problem
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+//
+// (In this case, all triangles must have surface greater than 0 to be accepted).
 
-    }
-
-}
