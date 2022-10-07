cab52187313c0f6064880e1326d561108f07b0e6
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-07 12:01:03 -0500

    moved files around to their correct location for grading

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,16 +1,17 @@
 "use strict";
-
-let username = pasali
-let password = pasali1
+// Lecture mini exercise
+let username = 'pasali'
+let password = 'pasali1'
 let userUsername = prompt('Enter your Username')
 let userPassword = prompt('Enter your Password')
 
-if (username === userUsername && password === userPassword){
-        console.log("User has succefully logged in");
-    else{
-        console.log('Credentials are incorrect');
-    }
+if (username === userUsername && password === userPassword) {
+    console.log("User has successfully logged in");
 }
+else{
+        console.log('Credentials are incorrect');
+}
+
 
 /* ########################################################################## */
 
