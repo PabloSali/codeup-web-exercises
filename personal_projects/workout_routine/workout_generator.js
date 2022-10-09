08d431d79d8e012f1c4a0dc024ff1d63b45ac68b
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-08 22:41:20 -0500

    worked on personal projects

--- personal_projects/workout_routine/workout_generator.js
+++ personal_projects/workout_routine/workout_generator.js
@@ -7,10 +7,10 @@
 */
 let armPulling =['DB Hammer Curl','Single KB Curl','DB Concentration Curl'];
 let armPushing =['DB Triceps Extension','KB Overhead Press','Kb Kickback' ];
-let legsMain =['Goblet Squat', -];
-let legsAux =[];
-let coreFocus = [];
-let backAndTraps = [];
-let chestAndShoulder = [];
-let cardioDay = [];
+let legsMain =['Goblet Squat', 'DB Lunges', 'KB Swings'];
+let legsAux =['Romanian DL', 'Weighted Calf Raise', 'Hip Thrusts'];
+let coreFocus = ['Leg Raise', 'Ankle Swings', 'Crunches'];
+let backAndTraps = ['DB Bent Rows', 'Pull Ups', 'Shrugs'];
+let chestAndShoulder = ['DB Bench Press', 'DB Close Grip Press', 'KB Squeeze Press'];
+let cardioDay = ['Sprints', '60/120', 'Endurance Run'];
 
