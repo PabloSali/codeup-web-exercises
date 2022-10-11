f81aba6b280f42718d67b2e880037739481286cf
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-11 09:33:53 -0500

    finalized warmup

--- js/warmup.js
+++ js/warmup.js
@@ -2,11 +2,9 @@
 //
 // Write a function that takes in a number and returns the number passed multiplied by the number 5. If we pass anything other than a number into the function, the function should return the number 0.
 
-function multiplyByFive(x){
-    if (typeOf x === number){
-    return x * 5;
-}
-    else {
+const multiplyByFive = (x) => {
+    if (parseFloat(x) !== x){
         return 0
-}
+    }
+    return x*5
 }
