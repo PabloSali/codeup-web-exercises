1a2e6cc4465c4e270fad5b12c787a9501a6b230c
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-11 09:25:21 -0500

    Oct 11 warmup

---
+++ js/warmup.js
@@ -0,0 +1,11 @@
+// Warm-up:
+//
+// Write a function that takes in a number and returns the number passed multiplied by the number 5. If we pass anything other than a number into the function, the function should return the number 0.
+
+function multiplyByFive(x){
+    if (typeOf x === number);
+    return x * 5;
+}
+    else {
+        return 0
+};
--- personal_projects/workout_routine/workout_generator.js
+++ personal_projects/workout_routine/workout_generator.js
@@ -8,7 +8,7 @@
 
 
 let armPulling =['DB Hammer Curl','Single KB Curl','DB Concentration Curl'];
-let armPushing =['DB Triceps Extension','KB Overhead Press','Kb Kickback' ];
+let armPushing =['DB Triceps Extension','KB Overhead Press','KB Kickback' ];
 let legsMain =['Goblet Squat', 'DB Lunges', 'KB Swings'];
 let legsAux =['Romanian DL', 'Weighted Calf Raise', 'Hip Thrusts'];
 let coreFocus = ['Leg Raise', 'Ankle Swings', 'Crunches'];
@@ -16,8 +16,9 @@ let backAndTraps = ['DB Bent Rows', 'Pull Ups', 'Shrugs'];
 let chestAndShoulder = ['DB Bench Press', 'DB Close Grip Press', 'KB Squeeze Press'];
 let cardioDay = ['Sprints', '60/120', 'Endurance Run'];
 
-let pushingDay = [armPushing];
-
+let pushingDay = [armPushing[Math.floor(Math.random() * 4)], legsMain[Math.floor(Math.random() * 4)], chestAndShoulder[Math.floor(Math.random() * 4)]];
+let pullingDay = [armPulling[Math.floor(Math.random() * 4)], legsAux[Math.floor(Math.random() * 4)], backAndTraps[Math.floor(Math.random() * 4)]];
+let runDay = [coreFocus[Math.floor(Math.random() * 4)], cardioDay[Math.floor(Math.random() * 4)]];
 function dailyWorkout (day){
 
-};
+}
