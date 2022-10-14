26294c4abe854dfa9502f8e5f2db238b10190968
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-14 14:08:25 -0500

    worked on iterating

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,41 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+       let names = ['John', 'Jacob', 'Jingle', 'Heimersmith'];
+    //-----------------------------------------------------------------
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+        console.log(names)
+    //-----------------------------------------------------------------
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    let i = 0
+    while(i < 4) {
+        console.log(names[i]);
+        i += 1;
+    }
+    //-----------------------------------------------------------------
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+       for(let i = 0; i < 4; i++){
+           console.log(names[i]);
+       }
+    //-----------------------------------------------------------------
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+        names.forEach(function)
+    //-----------------------------------------------------------------
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -44,4 +55,5 @@
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
 
+    //-----------------------------------------------------------------
 })();
