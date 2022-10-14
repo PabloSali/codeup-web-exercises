a89a1a01e2370cbd929a8adadeaa71c80b4e16b3
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-14 08:17:57 -0500

    Started loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,28 @@
+// Step 2
+function multiplicationTable(num){
+    console.log('7 x 1 = ' + (num * 1));
+    console.log('7 x 2 = ' + (num * 2));
+    console.log('7 x 3 = ' + (num * 3));
+    console.log('7 x 4 = ' + (num * 4));
+    console.log('7 x 5 = ' + (num * 5));
+    console.log('7 x 6 = ' + (num * 6));
+    console.log('7 x 7 = ' + (num * 7));
+    console.log('7 x 8 = ' + (num * 8));
+    console.log('7 x 9 = ' + (num * 9));
+    console.log('7 x 10 = ' + (num * 10));
+}
+//---------------------------------------------------------------
+// Step 3
+
+for(let i = 0; i < 20; ++i){
+    if (x % 2 === 0){
+        console.log(x + ' is even');
+    } else {
+        console.log(x + ' is odd');
+    }
+}
+
+//----------------------------------------------------------------
+// Step 4
+
+for
