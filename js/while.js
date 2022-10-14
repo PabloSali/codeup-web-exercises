b0856e62ce289a8f5d54e11be5cfb79b539bbaa8
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-14 13:43:44 -0500

    added arrays exercise documents

--- js/while.js
+++ js/while.js
@@ -2,3 +2,20 @@ num = 1;
 while(num < 65536){
     console.log(num *= 2);
 }
+
+//---------------------------------------------------------------------
+
+let conesNeededToSell = Math.floor(Math.random() * 50) + 50;
+do{
+    let clientBuying = Math.floor(Math.random() * 5) + 1;
+    let conesRemaining = conesNeededToSell - clientBuying
+    if(clientBuying <= conesRemaining){
+        console.log(clientBuying + ' cones sold');
+        conesNeededToSell -= clientBuying;
+    } else {
+        console.log('Sorry we do not have the inventory to sell that many.');
+    }
+} while (conesNeededToSell !== 0)
+
+console.log('We sold all our cones!')
+
