be210f062d3fbbdc171347848ec610fcbeec71c8
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-14 10:35:07 -0500

    fixed exercise after walkthrough example

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,17 @@
-let oddNum = prompt("Choose an Odd number between 1 -50.");
-
-for(let i = 0; i < 50; i++){
-
-}
+//
+// let userNumber = parseFloat(prompt('Choose a number between 1 and 50.'))
+//
+// if(userNumber % 2 === 0 ){
+//     alert('The number you chose was not odd')
+// }
+// for (let i = 1; i < 50; i++) {
+//
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     if (i === userNumber){
+//         continue;
+//     }
+//
+//     console.log('Here is an odd number: ' + i);
+// }
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,16 +1,10 @@
 // Step 2
 function multiplicationTable(num){
-    console.log('7 x 1 = ' + (num * 1));
-    console.log('7 x 2 = ' + (num * 2));
-    console.log('7 x 3 = ' + (num * 3));
-    console.log('7 x 4 = ' + (num * 4));
-    console.log('7 x 5 = ' + (num * 5));
-    console.log('7 x 6 = ' + (num * 6));
-    console.log('7 x 7 = ' + (num * 7));
-    console.log('7 x 8 = ' + (num * 8));
-    console.log('7 x 9 = ' + (num * 9));
-    console.log('7 x 10 = ' + (num * 10));
+    for(let i = 1; i <= 10; i++) {
+        console.log(num + " x " + i, " = " + num * i);
+    }
 }
+multiplicationTable(6);
 //---------------------------------------------------------------
 // Step 3
 
@@ -27,17 +21,14 @@ for(let i = 0; i < 20; ++i){
 // Step 4
 
 for(let i = 1; i < 10; i++){
-   let num = i
-    numToStr = num.toString()
+   let num = i;
+    let numToStr = num.toString();
     console.log(numToStr.repeat(num));
-
-    parseFloat(i);
 }
 
 //----------------------------------------------------------------
 // Step 5
 
 for(let i = 100; i > 0; i = i - 5){
-
-    console.log(i)
+    console.log(i);
 }
