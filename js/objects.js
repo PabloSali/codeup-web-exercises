0ae267e6206f258c3c1177e24f31210e66a16ed6
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-17 13:36:49 -0500

    finished 3rd problem on object exercise

--- js/objects.js
+++ js/objects.js
@@ -3,14 +3,16 @@
 
     /**
      * TODO:
-     * Create an object with firstName and lastName properties that are strings
-     * with your first and last name. Store this object in a variable named
-     * `person`.
+     * Create an object with firstName and lastName properties that are string with your first and last name. Store this object in a variable named`person`.
      *
      * Example:
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
+    let person = {
+        firstName: 'Pablo',
+        lastName: 'Salinas'
+    }
 
     /**
      * TODO:
@@ -21,10 +23,11 @@
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
-
+    person.sayHello = function(){
+        console.log('Hello from ' + person.firstName + ' ' + person.lastName);
+    }
     /** TODO:
-     * HEB has an offer for the shoppers that buy products amounting to
-     * more than $200. If a shopper spends more than $200, they get a 12%
+     * HEB has an offer for the shoppers that buy products amounting to more than $200. If a shopper spends more than $200, they get a 12%
      * discount. Write a JS program, using conditionals, that logs to the
      * browser, how much Ryan, Cameron and George need to pay. We know that
      * Cameron bought $180, Ryan $250 and George $320. Your program will have to
@@ -36,11 +39,19 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    let shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+shoppers.forEach(function (element){
+        if (element.amount >= 200) {
+            console.log(element.name + ' you spent ' + element.amount + '. Your discounted price is: ' + (element.amount * .12) + '. ' + 'Your new total is: ' + (element.amount - (element.amount * .12) + '.'));
+        } else {
+            console.log(element.name + ' you spent ' + element.amount + '. You did not meet the requirement for the discount. Your total is: ' + element.amount + '.');
+        }
+    });
+
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
