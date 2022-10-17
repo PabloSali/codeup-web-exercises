59bca748d24ab8e3eece38a525f2f25e53dbe75a
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-17 09:21:58 -0500

    completed warmup

--- js/warmup.js
+++ js/warmup.js
@@ -8,3 +8,18 @@ const multiplyByFive = (x) => {
     }
     return x*5
 }
+
+//---------------------------------------------------------
+//Morning Warm-Up:
+//
+// Create a function named "typePrinter" that accepts an array as an input, and logs the data type of each element to the console.
+//
+ let arr = [true, "Icon", 25, "66", false, 0];
+ let x = 0
+function typePrinter(array){
+     while (x < array.length){
+         console.log(typeof array[x])
+         x = x + 1
+     }
+}
+typePrinter(arr);
