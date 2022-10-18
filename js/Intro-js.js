019ea9c81352247899a09c70d7b68596070e93f4
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-18 13:18:11 -0500

    completed circle exercise

--- js/Intro-js.js
+++ js/Intro-js.js
@@ -1,14 +1,27 @@
-'use strict';
+// 'use strict';
+//
+// let greeting = 'Hello from an external source!';
+// console.log(greeting);
+//
+// let myAge = 25
+//
+// alert("You've been hacked!")
+//
+// let username = prompt("What is your name?");
+// console.log(username);
+//
+// let isFull = confirm('is the class full?');
+// console.log(isFull);
+function calculateChange(totalPaid, totalCost){
+    if(parseFloat(totalPaid) == totalPaid && parseFloat(totalCost) == totalCost){
+        let change = (totalPaid - totalCost);
+        let newChange = change.toFixed(2)
+        return '$' + newChange.toString();
+    }else{
+        return false;
+    }
+}
 
-let greeting = 'Hello from an external source!';
-console.log(greeting);
+console.log(calculateChange(23, 21));
 
-let myAge = 25
-
-alert("You've been hacked!")
-
-let username = prompt("What is your name?");
-console.log(username);
-
-let isFull = confirm('is the class full?');
-console.log(isFull);
+console.log('wow');
--- js/circle.js
+++ js/circle.js
@@ -6,18 +6,15 @@
         radius: 3,
 
         getArea: function () {
-            // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            return Math.PI * (circle.radius * circle.radius);
         },
 
         logInfo: function (doRounding) {
-            // TODO: complete this method.
-
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-
+            if(doRounding === true){
+                console.log(Math.round(this.getArea()))
+            } else{
+                console.log(this.getArea());
+            }
             console.log("Area of a circle with radius: " + this.radius + ", is: ");
         }
     };
@@ -29,9 +26,7 @@
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
-
-    // log info about the circle
+        circle.radius = 5
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
