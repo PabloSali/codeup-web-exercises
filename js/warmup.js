4dbdde0f406e4b4ba4e98af440a1d2c973d1142b
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-25 09:16:13 -0500

    completed js warmup

--- js/warmup.js
+++ js/warmup.js
@@ -2,29 +2,29 @@
 //
 // Write a function that takes in a number and returns the number passed multiplied by the number 5. If we pass anything other than a number into the function, the function should return the number 0.
 
-const multiplyByFive = (x) => {
-    if (parseFloat(x) !== x){
-        return 0
-    }
-    return x*5
-}
+// const multiplyByFive = (x) => {
+//     if (parseFloat(x) !== x){
+//         return 0
+//     }
+//     return x*5
+// }
 
 //---------------------------------------------------------
 //Morning Warm-Up:
 //
 // Create a function named "typePrinter" that accepts an array as an input, and logs the data type of each element to the console.
 //
- let arr = [true, "Icon", 25, "66", false, 0];
- let x = 0
-function typePrinter(array){
-     while (x < array.length){
-         console.log(typeof array[x])
-         x = x + 1
-     }
-}
-typePrinter(arr);
+//  let arr = [true, "Icon", 25, "66", false, 0];
+//  let x = 0
+// function typePrinter(array){
+//      while (x < array.length){
+//          console.log(typeof array[x])
+//          x = x + 1
+//      }
+// }
+// typePrinter(arr);
 
-
+//------------------------------------------------------------------------
 // Create a function named "average" that takes in an array of numeric values, and returns the average. If any of the elements in the array are non-numeric, the function should return false.
 //
 //     Ex:
@@ -36,24 +36,42 @@ typePrinter(arr);
 function isNumeric (){
     !isNaN(parseFloat(x));
 }
-function average(arrNum){
-    let arrNumAverage = 0
-             for(let i = 0; i < arrNum.length; i++) {
-            if (isNumeric(arrNum[i])) {
-                return false
-            }
-            arrNumAverage = parseFloat(arrNumAverage) + parseFloat(arrNum[i]);
+// function average(arrNum){
+//     let arrNumAverage = 0
+//              for(let i = 0; i < arrNum.length; i++) {
+//             if (isNumeric(arrNum[i])) {
+//                 return false
+//             }
+//             arrNumAverage = parseFloat(arrNumAverage) + parseFloat(arrNum[i]);
+//
+//             if (i === (arrNum.length - 1)){
+//                 arrNumAverage = arrNumAverage / (arrNum.length);
+//                 if(arrNumAverage === NaN) {
+//                     return false;
+//                 } else {
+//                     return arrNumAverage
+//                 }
+//             }
+//
+//         }
+// }
+// arrNum = [{name: "Codey the Duck"}, 10, 3, false];
+// console.log(average(arrNum))
 
-            if (i === (arrNum.length - 1)){
-                arrNumAverage = arrNumAverage / (arrNum.length);
-                if(arrNumAverage === NaN) {
-                    return false;
-                } else {
-                    return arrNumAverage
-                }
-            }
+//------------------------------------------------------------------------
+// Warm-up:
+//
+// Create a function named "filterNegativity" that accepts an array of numbers, and returns an array with only positive numbers.
+//
+//     Ex: filterNegativity([5, -6, 2, 0, -5, -13]) // returns [5, 2, 0,]
 
+function filterNegativity(arr){
+    for(i=0; i<arr.length;i++){
+        let posArray = [];
+        if(arr[i] >= 0 && isNumeric(arr[i]) === true) {
+            posArray.push[arr[i]];
+        } else if (i === arr.length){
+            return posArray;
         }
+    }
 }
-arrNum = [{name: "Codey the Duck"}, 10, 3, false];
-console.log(average(arrNum))
