142544f712d5fb45031ebd77422296d042937b97
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-16 12:13:04 -0600

    finished mouse event listener exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,13 +3,33 @@
 
 (function(){
     $(document).ready(function(){
+//---------------------------------------------------------------------
+//  Selectors
         // let h1contents = $('#head').html();
         // alert(h1contents);
 
-        $('.codeup').css('border', 'solid 1px red');
-
-        $('li').css('font-size',  ' 20px');
-        $('li, p , h1').css('background-color', 'yellow');
+        // $('.codeup').css('border', 'solid 1px red');
+        //
+        // $('li').css('font-size',  ' 20px');
+        // $('li, p , h1').css('background-color', 'yellow');
+//---------------------------------------------------------------------
+//  Mouse Event Listeners
+        // 1
+        $('h1').click(function() {
+            $('h1').css('background-color', 'yellow');
+        });
+        // 2
+        $('p').dblclick(function() {
+                $('p').css('font-size',  ' 18px');
+            });
+        // 3
+        $('li').hover(
+            function() {
+                $(this).css('color', 'red');
+            },
+            function() {
+                $(this).css('color', 'black');
+            });
 
     })
 })();
