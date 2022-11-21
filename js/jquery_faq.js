d72a59e0cb9eaaaec9110b201451c6b820a51bd8
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-21 15:40:00 -0600

    finished jquery_faq

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,9 +1,23 @@
 "use strict";
 
-
-
-
-$(document).keyup(function(event){
-    console.log(event.keyCode);
-
-});
+    // console.log(event.keyCode);
+// Shows and hides all dd items
+    $('#toggle').click(function(e) {
+        e.preventDefault();
+        $('dd').toggleClass('invisible');
+    })
+// Toggle highlighted class on dd items
+    $('dd').click(function(e) {
+        $(this).toggleClass('highlighted');
+    })
+// Button to highlight last element in the list
+    $('button').click(function(e) {
+        $('ul').each(function() {
+            $(this).children().last().toggleClass('highlighted');
+        })
+    })
+// Children list items bold when h3 is clicked
+    $('h3').click(function(e) {
+        let headingId = e.target.nextElementSibling.id;
+        $(`#${headingId}`).children().toggleClass('bold').css;
+    })
