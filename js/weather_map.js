9941694e9782be41a5a2faee2b0018d70ab09c99
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-28 11:14:51 -0600

    created css for weather map

--- js/weather_map.js
+++ js/weather_map.js
@@ -7,10 +7,35 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
     console.log(data); // Entire Object Console Log
     console.log(data.list[0].main.temp_max); // Max Temp Console Log
 });
+//------------- Map Box Code ----------------------
+
+function geocode(search, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
+
+function reverseGeocode(coordinates, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+        })
+        // to get all the data from the request, comment out the following three lines...
+        .then(function(data) {
+            return data.features[0].place_name;
+        });
+}
 
 
-
-
+//------------- Map Box Code ˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆ
 
 /* --------------------------------------
 Op Plan
