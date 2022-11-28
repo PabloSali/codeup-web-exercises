d66a80e1977b8b271123b6e5451e86db50d5076a
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-28 13:19:49 -0600

    started cards format

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,26 +1,49 @@
-
-$.get("http://api.openweathermap.org/data/2.5/forecast", {
-    APPID: OPEN_WEATHER_APPID,
-    q:     "San Antonio, US",
-    units: "imperial"
-}).done(function(data) {
-    console.log(data); // Entire Object Console Log
-    console.log(data.list[0].main.temp_max); // Max Temp Console Log
-});
+// Code to get object information from api
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: OPEN_WEATHER_APPID,
+        q:     "San Antonio, US",
+        units: "imperial"
+    }).done(function(data) {
+        console.log(data); // Entire Object Console Log
+        console.log(data.list[0].main.temp_max); // Max Temp Console Log
+    });
 //------------- Map Box Code ----------------------
-//---------- Creates Map ------------
-mapboxgl.accessToken = MAPBOX_TOKEN;
-let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 10,
-    center: [-96.80331225411656, 32.77771008020435]
-});
-
-
-
+    //---------- Creates Map ------------
+    mapboxgl.accessToken = MAPBOX_TOKEN;
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 10,
+        center: [-96.80331225411656, 32.77771008020435]
+    });
 
 //------------- Map Box Code ˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆ
+//------------- Forecast Card Code ˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆ
+
+// function makeForecastCard() {
+//
+//     let coffeeCards = '<div class="card coffeeCard" style="width: 18rem;">';
+//     coffeeCards += '<img src="' + coffee.image +'" className="card-img-top" alt="...">';
+//     coffeeCards += '<div class="card-body">'
+//     coffeeCards += '<h5 class="card-title headerFont"> '+ coffee.name +' </h5>'
+//     coffeeCards += '<p class="card-text">' + coffee.roast + ' roast</p>'
+//     coffeeCards += '</div>'
+//     coffeeCards += '</div>'
+//
+//     return coffeeCards;
+// }
+//
+// function place all(coffees) {
+//     let html = '';
+//     for(let i = 0; i < coffees.length; i++) {
+//         // if ()
+//         html += makeForecastCard(coffees[i]);
+//     }
+//     return html;
+// }
+//
+// let tbody = document.querySelector('#coffees');
+// tbody.innerHTML = renderCoffees(coffees);
 
 /* --------------------------------------
 Op Plan
