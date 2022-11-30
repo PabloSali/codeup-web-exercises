94e43336c6efc98b68ef9cda92a75d51257f5c3d
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-29 20:15:04 -0600

    created card info

--- js/weather_map.js
+++ js/weather_map.js
@@ -13,6 +13,21 @@
 // selector for search button and event listener that initiates user city search
 let userCitySearch = document.querySelector('#citySearchButton');
 userCitySearch.addEventListener('click', loadUserCity);
+// Forecast 1 Query Selectors
+let forecastOneTitle = document.querySelector('#forecast1-title');
+let forecastOneHigh = document.querySelector('#forecast1-temp-high');
+let forecastOneLow = document.querySelector('#forecast1-temp-low');
+let forecastOneFeel = document.querySelector('#forecast1-feel-temp');
+let forecastOneHum = document.querySelector('#forecast1-humidity');
+let forecastOneCov = document.querySelector('#forecast1-coverage');
+
+// Forecast 2 Query Selectors
+let forecastTwoTitle = document.querySelector('#forecast2-title');
+let forecastTwoHigh = document.querySelector('#forecast2-temp-high');
+let forecastTwoLow = document.querySelector('#forecast2-temp-low');
+let forecastTwoFeel = document.querySelector('#forecast2-feel-temp');
+let forecastTwoHum = document.querySelector('#forecast2-humidity');
+let forecastTwoCov = document.querySelector('#forecast2-coverage');
 
 let currentCityArray = []
 
@@ -29,26 +44,52 @@ function loadUserCity() {
     }).done(function (data) {
         let arrayData = [data]
         console.log(data); // Entire Object Console Log
-        console.log(data.list[0].main.temp_max); // Max Temp Console Log
         currentCityArray.push(arrayData);
         // console.log(currentCityArray)
         variableAssignment();
+        // renderCityMap();
     });
 };
+
+// function renderCityMap(){
+//     let currentCityInfo = currentCityArray[0];
+//     let cityLat = currentCityInfo[0].list[0].city.coord.lat;
+//     let cityLon = currentCityInfo[0].list[0].city.coord.lon;
+//     console.log(cityLat);
+//     console.log(cityLon);
+// };
+
+
 function variableAssignment() {
         // Shows all JSON for city
         console.log(currentCityArray);
         // Variable to use for accessing JSON, list index is date
         let currentCityInfo = currentCityArray[0];
+        // Variables assign data and push it to the Forecast Cards
+        // Day 1
+        let dayOneDate = currentCityInfo[0].list[0].visibility.dt_txt
         let dayOneMax = currentCityInfo[0].list[0].main.temp_max;
         let dayOneMin = currentCityInfo[0].list[0].main.temp_min;
         let dayOneFeel = currentCityInfo[0].list[0].main.feels_like;
         let dayOneHum = currentCityInfo[0].list[0].main.humidity;
+        let dayOneCov = currentCityInfo[0].list[0].weather[0].description;
+        console.log(dayOneCov);
+        forecastOneTitle.innerHTML = "Date: " + dayOneDate;
+        forecastOneHigh.innerHTML = "High: " + dayOneMax;
+        forecastOneLow.innerHTML = "Low: " + dayOneMin;
+        forecastOneFeel.innerHTML = "Feels Like: " + dayOneFeel;
+        forecastOneHum.innerHTML = "Humidity: " + dayOneHum;
+        forecastOneCov.inerHTML = "Coverage: " + dayOneCov;
 
-        console.log(dayOneMax);
-        console.log(dayOneMin);
-        console.log(dayOneFeel);
-        console.log(dayOneHum);
+        // Day 2
+        // let dayTwoMax = currentCityInfo[0].list[8].main.temp_max;
+        // let dayTwoMin = currentCityInfo[0].list[8].main.temp_min;
+        // let dayTwoFeel = currentCityInfo[0].list[8].main.feels_like;
+        // let dayTwoHum = currentCityInfo[0].list[8].main.humidity;
+        // forecastTwoHigh.innerHTML = "High: " + dayTwoMax;
+        // forecastTwoLow.innerHTML = "Low: " + dayTwoMin;
+        // forecastTwoFeel.innerHTML = "Feels Like: " + dayTwoFeel;
+        // forecastTwoHum.innerHTML = "Humidity: " + dayTwoHum;
 }
 
 //------------- Map Box Code ----------------------
